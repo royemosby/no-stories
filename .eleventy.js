a6617783659cceeab156a460ad79279d705a8ecc
Roy Mosby <ego.madking@gmail.com>
2020-03-15 22:20:12 -0400

    added concept collection w/ pinned entry priority

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,16 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/scripts');
   eleventyConfig.setDataDeepMerge(true);
 
+  eleventyConfig.addCollection('conceptsPlusPin', function(collection) {
+    let pinned = collection
+      .getFilteredByTag('concepts')
+      .filter(item => item.data.tags.includes('pinned'));
+    let nonPinned = collection
+      .getFilteredByTag('concepts')
+      .filter(item => !item.data.tags.includes('pinned'));
+    return [...pinned, ...nonPinned];
+  });
+
   return {
     dir: {
       input: 'src',
@@ -13,3 +23,14 @@ module.exports = function(eleventyConfig) {
     passthroughFileCopy: true,
   };
 };
+
+/*
+module.exports = function(eleventyConfig) {
+  
+  eleventyConfig.addCollection('conceptsPlusPin', function(collection) {
+    let pinned = collection.getFilteredByTagName('concepts').filter(item => item.data.tags === 'pinned');
+    let nonPinned = collection.getFilteredByTagName('concepts').filter(item => item.data.tags !== 'pinned');
+    return [...pinned, ...nonPinned];
+  });
+
+};*/
