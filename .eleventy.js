bbd4f65dbcad09960a28509cf08a279b0a9a966f
Roy Mosby <ego.madking@gmail.com>
2020-12-29 18:40:49 -0500

    alphabatize definitions

--- .eleventy.js
+++ .eleventy.js
@@ -20,6 +20,19 @@ module.exports = function(eleventyConfig) {
     return [...pinned, ...nonPinned];
   });
 
+  eleventyConfig.addCollection('glossarySortedAlpha', function(collection){
+    let sorted = collection.getFilteredByTag('definitions').sort(function(a, b){
+      if (a.data.title < b.data.title){ 
+        return -1;
+      } else if (a.data.title > b.data.title) {
+        return 1;
+      } else {
+        return 0
+    }
+    })
+    return sorted
+  });
+
   return {
     dir: {
       input: 'src',
