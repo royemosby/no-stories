c44b164357c19c0ae583043f5eeb8a1bf7b18c11
Roy Mosby <ego.madking@gmail.com>
2023-01-28 20:26:41 -0500

    create notes featur

--- .eleventy.js
+++ .eleventy.js
@@ -48,6 +48,18 @@ module.exports = function (eleventyConfig) {
     return collection.getFilteredByTag('recipes').filter((r)=> r.data.published === true);
   })
 
+  eleventyConfig.addCollection('notesSortedAlpha', function(collection){
+    return collection.getFilteredByTag('notes').sort(function (a, b) {
+      if (a.data.title < b.data.title) {
+        return -1;
+      } else if (a.data.title > b.data.title) {
+        return 1;
+      } else {
+        return 0
+      }
+    })
+  });
+
   return {
     dir: {
       input: 'src',
